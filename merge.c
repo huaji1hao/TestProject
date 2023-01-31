5e80b49b9e2c9ab635abc2a0abc5527cedaa4af9
huaji1hao <huaji1hao@gmail.com>
2023-01-31 20:02:42 +0800

    The file has two functions!

--- merge.c
+++ merge.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
+int multiply(int a, int b){
+    return a * b;
+}
+  
 int div(int a, int b){
     return a / b;
 }
 
 int main(){
-    printf("Hello World!\n");
+    printf("Hello World! * 2\n");
+    printf("I can code the function of mul!");
     printf("I can code the function of div!");
     return 0;
 }
