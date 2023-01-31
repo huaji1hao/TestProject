587be5012bce88dd47cc7b9f99d2340185ac090c
huaji1hao <huaji1hao@gmail.com>
2023-01-31 19:48:36 +0800

    The first function, multiply!

---
+++ mul.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int multiply(int a, int b){
+    return a * b;
+}
+int main(){
+    printf("Hello World!\n");
+    printf("I can code the function of mul!");
+    return 0;
+}
